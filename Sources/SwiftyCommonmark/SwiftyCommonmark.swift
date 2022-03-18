203d35e1aeebe17a56f3225b8c7c93546f8d87d3
OberstKrueger <ok@oberstkrueger.com>
2022-03-18 03:18:29 -0700

    Added basic conversion function

--- Sources/SwiftyCommonmark/SwiftyCommonmark.swift
+++ Sources/SwiftyCommonmark/SwiftyCommonmark.swift
@@ -1,3 +1,12 @@
-struct SwiftyCommonMark {
-    let text: String = "Hello, World!"
+import Foundation
+import cmark
+
+public extension String {
+    func markdownToHTML() -> String? {
+        if let processed = cmark_markdown_to_html(self, strlen(self), 0) {
+            return String(cString: processed)
+        } else {
+            return nil
+        }
+    }
 }
