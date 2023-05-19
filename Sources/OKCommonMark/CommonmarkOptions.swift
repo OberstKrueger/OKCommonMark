531d4a9646993d6e63eca1a9a1b591efccbc5fd7
OberstKrueger <ok@oberstkrueger.com>
2023-05-19 02:48:15 -0700

    Added documentation Removed unnecessay safe option

--- Sources/OKCommonMark/CommonmarkOptions.swift
+++ Sources/OKCommonMark/CommonmarkOptions.swift
@@ -1,9 +1,21 @@
+/// Available options for HTML rendering of Markdown.
 public enum CommonmarkOptions: Int32 {
+    /// Appends data-sourcepos attributes for all block elements.
     case sourcePosition = 1
+
+    /// Replaces soft breaks with hard breaks.
     case hardBreaks     = 2
-    case safe           = 3
+
+    /// Replaces soft breaks with spaces.
     case noBreaks       = 4
+
+    /// Checks for valid UTF8 within the String. Any illegal sequences will be replaed with U+FFFD.
     case validateUTF8   = 9
+
+    /// Converts quotation marks to curly quotation marks, -- to an en dash, and --- to an em dash.
     case smart          = 10
+
+    /// Renders raw HTML and all types of links (data:, file:, javascript:, etc.). Without this option, all raw HTML
+    /// contained in the String is removed from the resulting output.
     case unsafe         = 17
 }
--- Sources/OKCommonMark/SwiftyCommonMark.swift
+++ Sources/OKCommonMark/SwiftyCommonMark.swift
@@ -2,6 +2,10 @@ import Foundation
 import cmark
 
 public extension String {
+    /// Converts the String from Markdown to HTML. No verification of whether the Markdown is valid or if the string contains Markdown is performed.
+    ///
+    /// - Parameters:
+    ///     - options: The rendering options for HTML output.
     func markdownToHTML(_ options: CommonmarkOptions...) -> String? {
         let reduced: Int32 = options.reduce(0, {$0 + (1 << $1.rawValue)})
         var result: String?
